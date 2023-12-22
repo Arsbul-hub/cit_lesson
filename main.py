4cf438590c43abcad9cee2adfd1c44f7d4ae4d8c
arsbu <ars.bulygin@yandex.ru>
2023-12-22 21:07:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_max_number(): # определение функции
+    # Алгоритм нахождения максимума
+    numbers = [0, 1, -1, 5, 4, 2, 10, -100]
+    max_number = 0
+    for n in numbers:
+        if n > max_number:
+            max_number = n
+
+    print(max_number)
+
+get_max_number() # вызов (запуск) функции
+get_max_number() # вызов (запуск) функции
+get_max_number() # вызов (запуск) функции
+get_max_number() # вызов (запуск) функции
